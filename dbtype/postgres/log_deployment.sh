6df5912c55ae51aa1da8379110488d7039e23b5a
vagrant <vagrant@vagrant-cestel.dev.cmmint.net>
2015-07-21 14:56:28 -0400

    work on progress to simplify deployments db install

--- dbtype/postgres/log_deployment.sh
+++ dbtype/postgres/log_deployment.sh
@@ -6,34 +6,52 @@ function log_deployment() {
   _filename="$3"
   _state="$4"
 
-  ${db_binary} ${deployment_db} ${server_flag} ${user_flag} ${port_flag} -c "
+  _query_string="
   insert into deployment_tracker 
   ( 
     dbname, 
     deployment_type , 
     deployment_name , 
-    deployment_outcome,
-    deployed_by,
-    deployed_as,
-    reference_url
+    deployment_outcome"
+
+    if [ "${_deploy_db}" != "${deployments_db}" ]
+    then
+      _query_string="${_query_string},
+      deployed_by,
+      deployed_as,
+      reference_url"
+    fi
+  _query_string="${_query_string}
   )
   values 
   ( 
     '${_deploy_db}', 
     '${_change_type}', 
     '${_filename}', 
-    '${_state}',
-    '${deployed_by}',
-    '${deployed_as}',
-    '${change_control}'
+    '${_state}'
+    "
+    
+    if [ "${_deploy_db}" != "${deployments_db}" ]
+    then
+      _query_string="${_query_string},
+      '${deployed_by}',
+      '${deployed_as}',
+      '${change_control}'"
+    fi
+
+  _query_string="${_query_string}
   )
-  ;" > /dev/null 2>&1
+  ;"
+
+
+  ${db_binary} ${deployment_db} ${server_flag} ${user_flag} ${port_flag} -c "${_query_string}" > /dev/null 2>&1
   rc=$?
 
   unset _deploy_db
   unset _change_type
   unset _filename
   unset _state
+  unset _query_string
 
   if [ ${rc} -eq 0 ] 
   then
