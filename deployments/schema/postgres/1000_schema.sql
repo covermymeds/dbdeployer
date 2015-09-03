f38f03ea5e84eb52c960f09d61a37c37d8832a14
JP ODonnell <jodonnell@covermymeds.com>
2015-09-03 09:53:19 -0400

    Baseline of MSSQL and Postgres deployment tracker

--- deployments/schema/postgres/1000_schema.sql
+++ deployments/schema/postgres/1000_schema.sql
@@ -2,32 +2,15 @@
 -- PostgreSQL database dump
 --
 
-SET statement_timeout = 0;
-SET lock_timeout = 0;
-SET client_encoding = 'UTF8';
-SET standard_conforming_strings = on;
-SET check_function_bodies = false;
-SET client_min_messages = warning;
-
---
--- Name: plpgsql; Type: EXTENSION; Schema: -; Owner: 
---
 
 CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog;
 
 
---
--- Name: EXTENSION plpgsql; Type: COMMENT; Schema: -; Owner: 
---
 
 COMMENT ON EXTENSION plpgsql IS 'PL/pgSQL procedural language';
 
 
-SET search_path = public, pg_catalog;
 
---
--- Name: update_updated_at_column(); Type: FUNCTION; Schema: public; Owner: postgres
---
 
 CREATE FUNCTION update_updated_at_column() RETURNS trigger
     LANGUAGE plpgsql
@@ -39,15 +22,7 @@ CREATE FUNCTION update_updated_at_column() RETURNS trigger
 $$;
 
 
-ALTER FUNCTION public.update_updated_at_column() OWNER TO postgres;
 
-SET default_tablespace = '';
-
-SET default_with_oids = false;
-
---
--- Name: deployment_tracker; Type: TABLE; Schema: public; Owner: postgres; Tablespace: 
---
 
 CREATE TABLE deployment_tracker (
     id integer NOT NULL,
@@ -64,11 +39,7 @@ CREATE TABLE deployment_tracker (
 );
 
 
-ALTER TABLE deployment_tracker OWNER TO postgres;
 
---
--- Name: deployment_tracker_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
---
 
 CREATE SEQUENCE deployment_tracker_id_seq
     START WITH 1
@@ -78,29 +49,12 @@ CREATE SEQUENCE deployment_tracker_id_seq
     CACHE 1;
 
 
-ALTER TABLE deployment_tracker_id_seq OWNER TO postgres;
 
---
--- Name: deployment_tracker_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
---
 
 ALTER SEQUENCE deployment_tracker_id_seq OWNED BY deployment_tracker.id;
 
---
--- Name: update_updated_at_modtime; Type: TRIGGER; Schema: public; Owner: postgres
---
 
 CREATE TRIGGER update_updated_at_modtime BEFORE UPDATE ON deployment_tracker FOR EACH ROW EXECUTE PROCEDURE update_updated_at_column();
-
-
---
--- Name: public; Type: ACL; Schema: -; Owner: postgres
---
-
-REVOKE ALL ON SCHEMA public FROM PUBLIC;
-REVOKE ALL ON SCHEMA public FROM postgres;
-GRANT ALL ON SCHEMA public TO postgres;
-GRANT ALL ON SCHEMA public TO PUBLIC;
 
 
 --
