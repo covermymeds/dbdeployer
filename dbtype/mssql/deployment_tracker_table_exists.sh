e2de0f45bc44f8efe49dad5cddf45a89133e1085
CJ Estel <cestel@covermymeds.com>
2015-09-18 14:37:16 -0400

    added mssql deployment_tracker_table_exists

---
+++ dbtype/mssql/deployment_tracker_table_exists.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+deployment_tracker_table_exists() {
+
+  _table_exists=`${db_binary} -d ${deployment_db} ${server_flag}${port_flag} ${user_flag} ${password_flag} -h -1 -b -Q "
+  if OBJECT_ID('deployments.dbo.deployment_tracker') is not NULL
+select 't';"`
+
+  if [ "$_table_exists" = 't' ]
+  then
+    return 0
+  else
+    return 1
+  fi
+}
