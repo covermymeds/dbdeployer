7d0861163589a08c804e865764564111258ee01b
CJ Estel <cestel@covermymeds.com>
2015-09-18 15:06:48 -0400

    correced use case

--- dbtype/mssql/deploy_file.sh
+++ dbtype/mssql/deploy_file.sh
@@ -1,12 +1,13 @@
 function deploy_file() {
   _deploy_file="${1}"
+  # ${2} is the name of log file, which is optional
 
   deploy_output=$(${db_binary} -d ${dbname} ${server_flag}${port_flag} ${user_flag} ${password_flag} -h -1 -e -b -i "${fn_basedir}"/dbtype/"${dbtype}"/pre_deploy.sql "$_deploy_file" "${fn_basedir}"/dbtype/"$dbtype"/post_deploy.sql)
   rc=$?
 
   echo "${deploy_output}"
 
-  if [ -z ${2} ]
+  if ! [ -z ${2} ]
   then 
     echo "${deploy_output}" >> "${2}" 
   fi
@@ -21,7 +22,6 @@ function deploy_file() {
   fi
 
   unset _deploy_file
-  unset _logfile
 
   if [ ${rc} -eq 0 ]
   then
