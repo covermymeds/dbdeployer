33a083a7205412731eecd8edac836043ec834e9d
CJ Estel <cj@cjestel.net>
2015-10-02 15:05:40 -0400

    bug fixes

--- dbtype/postgres/deploy_file.sh
+++ dbtype/postgres/deploy_file.sh
@@ -3,7 +3,7 @@ function deploy_file() {
   _deploy_file="${1}"
   if ! [ -z "${2}" ]
   then 
-    _logfile="-L ${2}"
+    _logfile="-L${2}"
   fi
 
   ${db_binary} ${dbname} ${server_flag} ${user_flag} ${port_flag} -1 -X -v 'timing' -v 'ON_ERROR_STOP=1' -a -f "${_deploy_file}" "${_logfile}"
