5c9e76737890c71832431e36d4ebf79156edf65d
CJ Estel <cj@cjestel.net>
2016-01-07 17:03:14 -0500

    fixed logging issue

--- dbtype/mysql/deploy_file.sh
+++ dbtype/mysql/deploy_file.sh
@@ -4,12 +4,19 @@ function deploy_file() {
   _deploy_file="${2}"
   if ! [ -z "${3}" ]
   then 
-    _logfile="--tee='${3}'"
+    _logfile="${3}"
   fi
 
-  ${db_binary} -D ${_db_to_deploy} ${server_flag} ${user_flag} ${port_flag} -vvv --skip-pager < "${_deploy_file}"
+  deploy_output=`${db_binary} -D ${_db_to_deploy} ${server_flag} ${user_flag} ${port_flag} -vvv --skip-pager < "${_deploy_file}"`
   rc=$?
-  
+ 
+  echo "${deploy_output}"
+
+  if ! [ -z "${_logfile}" ]
+  then
+    echo "${deploy_output}" >> "${_logfile}"
+  fi
+
   unset _db_to_deploy
   unset _deploy_file
   unset _logfile
